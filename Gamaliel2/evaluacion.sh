f16db4b7c261645839bcfc95b245a894eb5482bb
gxguerra12 <g.guerra@duocuc.cl>
2024-05-23 00:37:03 +0000

    Jenkisn

--- Gamaliel2/evaluacion.sh
+++ Gamaliel2/evaluacion.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
 
-echo "FROM python" >> Gamaliel2/Dockerfile
-echo "RUN pip install flask" >> Gamaliel2/Dockerfile
-echo "COPY  ./static /home/myapp/static/" >> Gamaliel2/Dockerfile
-echo "COPY  ./templates /home/myapp/templates/" >> Gamaliel2/Dockerfile
-echo "COPY  ev2.py /home/myapp/" >> Gamaliel2/Dockerfile
-echo "EXPOSE 5040" >> Gamaliel2/Dockerfile
-echo "CMD python /home/myapp/ev2.py" >> Gamaliel2/Dockerfile
+cp ev2.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
 
-cd Gamaliel2
+echo "FROM python" >> tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY  ev2.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 5040" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/ev2.py" >> tempdir/Dockerfile
+
+cd tempdir
 docker build -t sampleapp .
 docker run -t -d -p 5040:5040 --name samplerunning sampleapp
 docker ps -a 
+ 
